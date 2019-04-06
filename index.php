1fcd26cb90bac722f07f7f0188faab76d3bfd745
Raziel <razzziel001@gmail.com>
2019-04-06 16:38:11 -0500

    Comenzando curso php

---
+++ index.php
@@ -0,0 +1,15 @@
+
+    <?php
+
+    $frutas = array("manzanas","banano","uvas");
+  /*  echo $frutas[2];
+
+    if ($frutas[1]=="banano") {
+      echo "CORRECTO";
+    }else {
+      echo "INCORRECTO";
+    }
+*/
+    $fruta2 = array("clave1"=> "manzana", "clave2"=>"uvas");
+    echo $fruta2["clave2"];
+     ?>
