8328ac78dbdd423bf30b311e8fd0a6f6fdeabbef
Raziel <razzziel001@gmail.com>
2019-04-09 12:21:34 -0500

    Sintaxis de constantes y ciclo if

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 
     <?php
+//arrrays
 
     $frutas = array("manzanas","banano","uvas");
   /*  echo $frutas[2];
@@ -10,6 +11,22 @@
       echo "INCORRECTO";
     }
 */
+
+
     $fruta2 = array("clave1"=> "manzana", "clave2"=>"uvas");
     echo $fruta2["clave2"];
+
+//constantes
+    define('constante','pi');
+    echo constante;
+
+//ciclos 
+$edad = 18;
+
+if($edad == 18){
+  echo "Es mayor de edad";
+}else{
+  echo "Es menor de edad";
+}
+
      ?>
