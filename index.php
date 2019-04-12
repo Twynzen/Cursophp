0bd1509befab8783ceefeb3fd6a40817eaa9c860
Raziel <razzziel001@gmail.com>
2019-04-12 14:08:54 -0500

    ejemplos de ciclo for y while

--- index.php
+++ index.php
@@ -14,34 +14,49 @@
 
 
     $fruta2 = array("clave1"=> "manzana", "clave2"=>"uvas");
+    echo "\n";
     echo $fruta2["clave2"];
+    echo "\n";
 
 //constantes
     define('constante','pi');
     echo constante;
+    echo "\n";
 
 //ciclos
 $edad = 18;
 
 if($edad == 18){
-  echo "Es mayor de edad";
+  echo "Es mayor de edad\n";
 }else{
-  echo "Es menor de edad";
+  echo "Es menor de edad\n";
 }
 $opc= 2;
 switch ($opc) {
   case 2:
-    echo "La variable es 2 ";
+    echo "La variable es 2 \n";
     break;
   case 1:
-  echo "La variable no es 2";
+  echo "La variable no es 2\n";
     break;
 
   default:
     # code...
     break;
 }
+echo "<br>";
+//ciclo for
+for ($i=0; $i < 10; $i++) {
+  echo "\n variable incrementa".$i;
+  echo "<br>";
+}
 
+$x = 0;
+while ($x <= 10) {
+echo $x;
+echo "<br>";
+ $x++;
+}
 
 
 
